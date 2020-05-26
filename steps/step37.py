89d61fea6acb66b545cc4a57d9f4619d3b87e100
daisukeokamoto1023 <okadaih11023@gmail.com>
2020-05-26 12:31:17 +0900

    finish step37

---
+++ steps/step37.py
@@ -0,0 +1,17 @@
+if '__file__' in globals():
+    import os
+    import sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from dezero import Variable
+    from dezero import setup_variable
+    from dezero.utils import plot_dot_graph
+    import dezero.functions as F
+setup_variable()
+
+if __name__ == '__main__':
+    x = Variable(np.array([[1, 2, 3], [4, 5, 6]]))
+    c = Variable(np.array([[10, 20, 30], [40, 50, 60]]))
+    t = x + c
+    print(t)
