dcb0b2a9d9b36b2d8f79a966e0a7bb383f0e05cc
daisukeokamoto1023 <okadaih11023@gmail.com>
2020-05-26 18:49:40 +0900

    finish step43

--- dezero/functions.py
+++ dezero/functions.py
@@ -132,6 +132,43 @@ class MeanSquaredError(Function):
         return gx0, gx1
 
 
+class Exp(Function):
+    def forward(self, x):
+        y = np.exp(x)
+        return y
+
+    def backward(self, gy):
+        y = self.outputs[0]()
+        gx = gy * y
+        return gx
+
+
+class Linear(Function):
+    def forward(self, x, W, b):
+        y = x.dot(W)
+        if b is not None:
+            y += b
+        return y
+
+    def backward(self, gy):
+        x, W, b = self.inputs
+        gb = None if b.data is None else sum_to(gy, b.shape)
+        gx = matmul(gy, W.T)
+        gW = matmul(x.T, gy)
+        return gx, gW, gb
+
+
+class Sigmoid(Function):
+    def forward(self, x):
+        y = np.tanh(x * 0.5) * 0.5 + 0.5
+        return y
+
+    def backward(self, gy):
+        y = self.outputs[0]()
+        gx = gy * y * (1 - y)
+        return gx
+
+
 def sin(x):
     return Sin()(x)
 
@@ -164,6 +201,22 @@ def broadcast_to(x, shape):
     return BroadcastTo(shape)(x)
 
 
+def linear_simple(x, W, b=None):
+    x, W = as_variable(x), as_variable(W)
+    t = matmul(x, W)
+    if b is None:
+        return t
+    y = t + b
+    t.data = None
+    return y
+
+
+def sigmoid_simple(x):
+    x = as_variable(x)
+    y = 1 / (1 + exp(-x))
+    return y
+
+
 def sum_to(x, shape):
     if x.shape == shape:
         return as_variable(x)
@@ -176,3 +229,15 @@ def matmul(x, W):
 
 def mean_squared_error(x0, x1):
     return MeanSquaredError()(x0, x1)
+
+
+def exp(x):
+    return Exp()(x)
+
+
+def sigmoid(x):
+    return Sigmoid()(x)
+
+
+def linear(x, W, b=None):
+    return Linear()(x, W, b)
